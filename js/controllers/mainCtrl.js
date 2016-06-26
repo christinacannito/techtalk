1b848a651a20af6685ba113ffdb53e2cf7e45efd
christinacannito <christinacannito@gmail.com>
2016-06-26 16:41:24 -0400

    functionality added, starting to add content

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,6 +1,14 @@
 app.controller('mainCtrl', function($scope){
 	$scope.show = false;
+	$scope.tut = false;
+	$scope.res = false;
 	$scope.showDivs = function() {
 		$scope.show = true;
+	}
+	$scope.showTutorial = function() {
+		$scope.tut = true;
+	}
+	$scope.showResources = function() {
+		$scope.res = true;
 	}
 })
