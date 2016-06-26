a8ff6c09d9eb6ceb97b5d4c59636f268710e9205
christinacannito <christinacannito@gmail.com>
2016-06-26 15:43:13 -0400

    added files for angular and started working on functionality

---
+++ js/controllers/mainCtrl.js
@@ -0,0 +1,6 @@
+app.controller('mainCtrl', function($scope){
+	$scope.show = false;
+	$scope.showDivs = function() {
+		$scope.show = true;
+	}
+})
--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 var app = angular.module('hour', ['ngRoute']);
 
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/tutorial', {
+		templateUrl: '../views/tutorial.htm'
+	})
+	.when('/resources', {
+		templateUrl: '../views/resources.htm'
+	});
+});
